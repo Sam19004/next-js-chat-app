4fdb3ac1791b61030e901d9ee21cce3ee08c2025
Sam19004 <19ce004@svitvasad.ac.in>
2022-10-24 02:56:52 +0530

    commit2

---
+++ whatsapp2/components/Sidebar.js
@@ -0,0 +1,31 @@
+import { Avatar,IconButton } from "@mui/material"
+import styled from "styled-components";
+import ChatIcon from '@mui/icons-material/Chat';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+    function Sidebar() {
+        return (
+            <Container>
+                <Header>
+                    <UserAvatar />
+                    <IconContainer>
+                        <IconButton>
+                        <ChatIcon/>
+                        </IconButton>
+                        <IconButton>
+                        <MoreVertIcon/>
+                        </IconButton>
+                       
+                    </IconContainer>
+                </Header>
+            </Container>
+        );
+    }
+export default Sidebar;
+const Container = styled.div``;
+const Header = styled.div``;
+const UserAvatar = styled(Avatar)`
+margin-top: 10px;
+margin-left: 14px;
+`;
+
+const IconContainer= styled.div``;
---
+++ whatsapp2/pages/chat.js
@@ -0,0 +1,8 @@
+function chat(){
+    return(
+        <div>
+            <h1>hello chat</h1>
+        </div>
+    );
+}
+export default chat;
