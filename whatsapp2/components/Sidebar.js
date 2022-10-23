fbd7aa94845b6b277ef0120caf42cad7a07b7d3c
Sam19004 <19ce004@svitvasad.ac.in>
2022-10-24 02:58:07 +0530

    1

--- whatsapp2/components/Sidebar.js
+++ whatsapp2/components/Sidebar.js
@@ -1,25 +1,25 @@
-import { Avatar,IconButton } from "@mui/material"
+import { Avatar, IconButton } from "@mui/material"
 import styled from "styled-components";
 import ChatIcon from '@mui/icons-material/Chat';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-    function Sidebar() {
-        return (
-            <Container>
-                <Header>
-                    <UserAvatar />
-                    <IconContainer>
-                        <IconButton>
-                        <ChatIcon/>
-                        </IconButton>
-                        <IconButton>
-                        <MoreVertIcon/>
-                        </IconButton>
-                       
-                    </IconContainer>
-                </Header>
-            </Container>
-        );
-    }
+function Sidebar() {
+    return (
+        <Container>
+            <Header>
+                <UserAvatar />
+                <IconContainer>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+
+                </IconContainer>
+            </Header>
+        </Container>
+    );
+}
 export default Sidebar;
 const Container = styled.div``;
 const Header = styled.div``;
@@ -28,4 +28,4 @@ margin-top: 10px;
 margin-left: 14px;
 `;
 
-const IconContainer= styled.div``;
+const IconContainer = styled.div``;
